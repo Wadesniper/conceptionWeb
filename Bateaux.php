d01724a1f0a6358842ffdda893c0e4698b0861a6
serigne babacar wade <babacarw825@gmail.com>
2018-11-22 21:18:26 +0000

    Upload New File

---
+++ Bateaux.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"> </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <link rel="stylesheet"  href="style.css" />
+ 
+    </head>
+    
+    <body>
+        
+            <div class="row">
+                <div class="col-md-4">
+                        <div class="card">
+                                <img class="card-img-top" src="americo.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">Amerigo Vespucci</h5>
+                                  <p class="card-text"></p>
+                                  <a href="register1.php" class="btn btn-primary">s'inscrire pour voir les info détaillées</a>
+                                </div>
+                        
+                       </div>
+               </div>
+
+        <br/>
+                <div class="col-md-4">
+                        <div class="card">
+                                <img class="card-img-top" src="kruzen.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">kruzenshten</h5>
+                                  <p class="card-text"></p>
+                                  <a href="register1.php" class="btn btn-primary">s'inscrire pour voir les info détaillées</a>
+                                </div>
+                        
+                         </div>
+                </div>
+
+                <div class="col-md-4">
+                        <div class="card">
+                                <img class="card-img-top" src="galeon.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">el Galeon</h5>
+                                  <p class="card-text"></p>
+                                  <a href="register1.php" class="btn btn-primary">s'inscrire pour voir les info détaillées</a>
+                                </div>
+                        
+                         </div>
+                </div>
+                <div class="col-md-4">
+                        <div class="card">
+                                <img class="card-img-top" src="belem.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">Belem</h5>
+                                  <p class="card-text"></p>
+                                  <a href="register1.php" class="btn btn-primary">s'inscrire pour voir les info détaillées</a>
+                                </div>
+                        
+                         </div>
+                </div>
+                <div class="col-md-4">
+                        <div class="card">
+                                <img class="card-img-top" src="mir.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">Mir</h5>
+                                  <p class="card-text"></p>
+                                  <a href="register1.php" class="btn btn-primary">s'inscrire pour voir les info détaillées</a>
+                                </div>
+                        
+                         </div>
+                </div>
+                <div class="col-md-4">
+                        <div class="card">
+                                <img class="card-img-top" src="christian.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">christian Raddish</h5>
+                                  <p class="card-text"></p>
+                                  <a href="register1.php" class="btn btn-primary">s'inscrire pour voir les info détaillées</a>
+                                </div>
+                        
+                         </div>
+                </div>
+                
+         </div>          
+        
+    </body>
+</html>
